687c54ded7bd4226d24c625905a5af8c89e0be3c
kbobocha <81590702+kbobocha@users.noreply.github.com>
2021-03-29 18:44:42 -0400

    Update swap.s

--- swap.s
+++ swap.s
@@ -12,12 +12,17 @@ swap:
 		# You may only use the three caller-saved registers: %eax, %ecx, and %edx.
 		# You also may not store data in any new memory locations.
 
-	movl 0x8(%ebp), %ecx  #load x into ecx
-	movl 0xc(%ebp), %edx		#load y into ecx
-	movl %ecx, %eax    #load *x to eax
-	movl (%edx), %ecx   #store *y
-	movl %eax, %edx  #store *x
-	movl (%edx), %edx 
+	movl 0x8(%ebp), %ecx  #load *x into ecx
+	movl 0xc(%ebp), %edx		#load *y into ecx
+	movl (%ecx), %eax #loads x to eax
+	movl %eax, (%edx) #loads x to edx
+	movl (%edx), %eax #loads y to eax
+	movl %eax, (%edx) #loads x to edx
+	
+	#movl %ecx, %eax    #load *x to eax
+	#movl (%edx), %ecx   #store *y
+	#movl %eax, %edx  #store *x
+	#movl (%edx), %edx 
 
 	#movl %ecx, 0x8(%ebp)  #store y
 	#movl %edx, 0x12(%ebp) #store x
