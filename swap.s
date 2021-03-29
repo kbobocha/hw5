5d6f823ad273491d491919a54157e853ee744819
kbobocha <81590702+kbobocha@users.noreply.github.com>
2021-03-29 19:30:27 -0400

    Update swap.s

--- swap.s
+++ swap.s
@@ -12,33 +12,21 @@ swap:
 		# You may only use the three caller-saved registers: %eax, %ecx, and %edx.
 		# You also may not store data in any new memory locations.
 
-	movl 0x8(%ebp), %ecx  #load *x into ecx
-	movl 0xc(%ebp), %edx		#load *y into ecx
+	movl 0x8(%ebp), %ecx 	 #load *x into ecx
+	movl 0xc(%ebp), %edx	#load *y into ecx
 	
-	movl (%ecx), %eax #loads x to eax
-	movl (%edx), %ecx #loads y to ecx
-	movl %eax, (%edx) #loads x to edx
-	movl %ecx, %eax #loads y to eax
-	movl %edx, %ecx #loads *y to ecx
-	movl %eax, (%ecx) #loads y to ecx
+	movl (%ecx), %eax #loads *x to eax
+	movl (%edx), %ecx #loads *y to ecx
+	movl %ecx, (%ecx) #changes *x to *y 
+	movl %eax, (%edx) #changes *y to *x 
 	
-	
-	#movl %ecx, %eax #loads y to ecx
+	#movl %ecx, %eax #loads y to eax
+	#movl %edx, %ecx #loads *y to ecx
 	#movl %eax, (%ecx) #loads y to ecx
+	#movl (%ecx), %eax #loads x to eax
 	
 	
 	
-	
-	
-	#movl %ecx, %eax    #load *x to eax
-	#movl (%edx), %ecx   #store *y
-	#movl %eax, %edx  #store *x
-	#movl (%edx), %edx 
-
-	#movl %ecx, 0x8(%ebp)  #store y
-	#movl %edx, 0x12(%ebp) #store x
-
-
 
 	leave                     # tear down swap's stack frame and return
 	ret
